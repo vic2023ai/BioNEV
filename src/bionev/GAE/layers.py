6287a9d21f0feb85078a4b6806b4da92269b84eb
vic2023ai <167651285+vic2023ai@users.noreply.github.com>
2024-05-27 13:32:31 -0300

    Update layers.py
    tf.random_uniform por tf.random.uniform

--- src/bionev/GAE/layers.py
+++ src/bionev/GAE/layers.py
@@ -24,7 +24,7 @@ def dropout_sparse(x, keep_prob, num_nonzero_elems):
     """
     noise_shape = [num_nonzero_elems]
     random_tensor = keep_prob
-    random_tensor += tf.random_uniform(noise_shape)
+    random_tensor += tf.random.uniform(noise_shape)
     dropout_mask = tf.cast(tf.floor(random_tensor), dtype=tf.bool)
     pre_out = tf.sparse_retain(x, dropout_mask)
     return pre_out * (1. / keep_prob)
