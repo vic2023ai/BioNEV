a53833c9db4307d3fb029a19b90898fda4946d52
vic2023ai <167651285+vic2023ai@users.noreply.github.com>
2024-06-02 13:45:18 -0300

    Update grarep.py
    solved k value problem

--- src/bionev/OpenNE/grarep.py
+++ src/bionev/OpenNE/grarep.py
@@ -22,33 +22,27 @@ class GraRep(object):
         for edge in self.g.G.edges():
             adj[look_up[edge[0]]][look_up[edge[1]]] = 1.0
             adj[look_up[edge[1]]][look_up[edge[0]]] = 1.0
-        # ScaleSimMat
-        # print('finish getAdjMat')
         return np.matrix(adj)
 
     def GetProbTranMat(self, Ak):
-        # print(np.sum(Ak, axis=0))
         tileMat = np.tile(np.sum(Ak, axis=0), (self.node_size, 1))
-        # print(np.min(tileMat))
         probTranMat = np.log(Ak / tileMat) - np.log(1.0 / self.node_size)
         probTranMat[probTranMat < 0] = 0
-        probTranMat[probTranMat == np.nan] = 0
+        probTranMat[np.isneginf(probTranMat)] = 0
+        probTranMat[np.isnan(probTranMat)] = 0
         return probTranMat
 
     def GetRepUseSVD(self, probTranMat, alpha):
-        # U, S, VT = la.svd(probTranMat)
+        k = min(self.dim, min(probTranMat.shape) - 1)
+        if k <= 0:
+            raise ValueError("El valor ajustado de 'k' no es válido. Verifique el tamaño del grafo y la dimensión deseada.")
 
-        U, Sigma, VT = svds(probTranMat, self.dim)
-        # print("finish svd..")
+        U, Sigma, VT = svds(probTranMat, k=k)
         Sigma = np.diag(Sigma)
         W = np.matmul(U, np.power(Sigma, alpha))
         C = np.matmul(VT.T, np.power(Sigma, alpha))
-        # print(np.sum(U))
         embeddings = W + C
         return embeddings
-        # Ud = U[:, 0:self.dim]
-        # Sd = S[0:self.dim]
-        # return np.array(Ud)*np.power(Sd, alpha).reshape((self.dim))
 
     def save_embeddings(self, filename):
         fout = open(filename, 'w')
@@ -66,16 +60,10 @@ class GraRep(object):
         for i in range(self.Kstep):
             print('Kstep =', i)
             self.Ak = np.dot(self.Ak, self.adj)
-            # print('finish np.dot(self.Ak, self.adj)')
             probTranMat = self.GetProbTranMat(self.Ak)
-            # print('finish GetProbTranMat')
             Rk = self.GetRepUseSVD(probTranMat, 0.5)
-            # print('finish GetRepUseSVD')
             Rk = normalize(Rk, axis=1, norm='l2')
-            # print('finish normalize')
             self.RepMat[:, self.dim * i:self.dim * (i + 1)] = Rk[:, :]
-            # print('finish RepMat[:, self.dim*i:self.dim*(i+1)] = Rk[:, :]')
-        # get embeddings
         self.vectors = {}
         look_back = self.g.look_back_list
         for i, embedding in enumerate(self.RepMat):
